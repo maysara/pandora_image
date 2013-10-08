10d46e3dd60c7a8495324698e43257b51ee71e2a
wafaa <wafaa.mohamed@arabdigitalexpression.org>
2013-10-08 17:50:05 +0200

    cleanup of excludes

--- bin/django-admin.py
+++
@@ -1,7 +0,0 @@
-#!/srv/pandora/bin/python
-# EASY-INSTALL-DEV-SCRIPT: 'Django==1.4.7','django-admin.py'
-__requires__ = 'Django==1.4.7'
-from pkg_resources import require; require('Django==1.4.7')
-del require
-__file__ = '/srv/pandora/src/django/django/bin/django-admin.py'
-execfile(__file__)
